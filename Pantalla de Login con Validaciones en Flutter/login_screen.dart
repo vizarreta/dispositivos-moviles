3baff50be8dcadcff25c7918a8724ee6f0353375
carlos adrian vizarreta <cvizarretac@ulasalle.edu.pe>
2025-11-19 14:35:26 -0500

    subiendo archivo pantalla de login

---
+++ Pantalla de Login con Validaciones en Flutter/login_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'user_list_screen.dart'; // Importar LISTA de Actividad Anterior
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _email = '';
+  String _password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+
+              // EMAIL
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Correo',
+                  prefixIcon: Icon(Icons.email),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'El correo es obligatorio';
+                  }
+                  if (!value.contains('@')) {
+                    return 'Debe ser un correo válido';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _email = value!,
+              ),
+
+              const SizedBox(height: 16),
+
+              // PASSWORD
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Contraseña',
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'La contraseña es obligatoria';
+                  }
+                  if (value.length < 6) {
+                    return 'Debe tener al menos 6 caracteres';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _password = value!,
+              ),
+
+              const SizedBox(height: 24),
+
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => UserListScreen(),
+                      ),
+                    );
+                  }
+                },
+                child: const Text('Ingresar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
